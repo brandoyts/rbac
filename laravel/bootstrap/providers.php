5edce2c9d2c27a295d9e71b4044f6ed3da719229
Brando Endona <cbrandoendona@gmail.com>
2025-08-10 13:59:46 +0800

    register service providers

--- laravel/bootstrap/providers.php
+++ laravel/bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
+    App\Providers\CoreServiceProvider::class,
+    App\Providers\UserServiceProvider::class,
+    App\Providers\AuthenticationServiceProvider::class,
 ];
